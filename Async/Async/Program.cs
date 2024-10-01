11d8a921abfd95fb4f5eb038b8c0db727b655964
NdizeyeArmand <armand-gael.ndizeye@student.ehb.be>
2024-10-01 10:37:54 +0200

    create Async

---
+++ Async/Async/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        API();
+        Console.ReadLine();
+    }
+
+    public static async void API()
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("API called");
+    }
+}
